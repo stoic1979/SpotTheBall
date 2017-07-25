d7257fe38f80c5067d0f7765bce67b7f97f737fb
Navjot Singh <weavebytes@gmail.com>
2017-07-25 12:28:45 +0530

    updated files

--- app.py
+++ app.py
@@ -26,33 +26,12 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/<filename>')
 def uploaded_file(filename):
-    print '==========++++++++++++++++++++', file_path
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-@app.route('/add', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['pic']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
 
 
 @app.route('/admin')
@@ -65,9 +44,8 @@ def admin():
 def add_game():
     ret = {}
     try:
-        # name = ''
-        # pic = allowed_file('pic')
         file = request.files['pic']
+
         # if user does not select file, browser also
         # submit a empty part without filename
         if file.filename == '':
@@ -75,15 +53,11 @@ def add_game():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            name = file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            print '=====================================', filename
-            pic = filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            pic = '%s/%s' % (file_path, filename)
 
-            #return redirect(url_for('uploaded_file',
-             #                        filename=filename))
+            # return redirect(url_for('uploaded_file', filename=filename))
 
-        # pic = up
-        print '=========+++++++++++++++++==============', pic
         x1 = request.form['x1']
         y1 = request.form['y1']
         x2 = request.form['x2']
@@ -101,7 +75,8 @@ def add_game():
         ret['error'] = 1
         ret['msg'] = exp
         print(traceback.format_exc())
-    return json.dumps(ret)
+    # return json.dumps(ret)
+    return redirect(url_for('uploaded_file', filename=filename))
 
 
 @app.route("/get_game", methods=['GET'])
