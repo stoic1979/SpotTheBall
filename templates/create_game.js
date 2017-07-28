38fd5fceb4f6db1c4a408d46f5b031659f9dc4a8
Navjot Singh <weavebytes@gmail.com>
2017-07-28 21:28:21 +0530

    draw eye rect and lines to ball

--- templates/create_game.js
+++ templates/create_game.js
@@ -3,7 +3,6 @@ var ctx;
 var imgBg;
 var imgBall;
 
-var eyes 	 	= [];
 var ballX 		= 100;
 var ballY 		= 100;
 var ballWidth   = 80;
@@ -11,6 +10,33 @@ var ballHeight  = 80;
 
 var curIndex;
 var curType = "";
+
+//####################//
+//                    //
+//        class       //
+//                    //
+//####################//
+function Point(x,y) 
+{
+    this.x = x;
+    this.y = y;
+}
+
+
+//-----------------------------------------------
+// initially we have 8 eyes with default values
+//-----------------------------------------------
+var eyes = [
+	 new Point(-1, -1),
+ 	 new Point(-1, -1),
+	 new Point(-1, -1),
+	 new Point(-1, -1),
+	 new Point(-1, -1),
+	 new Point(-1, -1),
+	 new Point(-1, -1),
+	 new Point(-1, -1)
+];
+
 
 
 function setupGameCreator() {
@@ -43,6 +69,28 @@ function drawScene() {
     ctx.drawImage(imgBg, 0, 0);
 
     ctx.drawImage(imgBall, ballX, ballY);
+
+
+    //#########################################//
+    //                                         //
+    //         draw eye rect and               //
+    //         eye lines to ball               //
+    //                                         //
+    //#########################################//
+    for(var i=0; i < eyes.length; i++) {
+        var eye = eyes[i];
+
+		if(eye.x == -1 || eye.y == -1) continue;
+
+		console.log("EYE: " + eye.x + "," + eye.y);
+
+		ctx.rect(eye.x-5, eye.y-5, 10, 10);
+        ctx.stroke();
+
+        ctx.moveTo(eye.x, eye.y);
+        ctx.lineTo(ballX + ballWidth/2, ballY + ballHeight/2);
+        ctx.stroke();
+    }
 }
 
 
@@ -61,16 +109,19 @@ function onClick(evt) {
 		if(curType == "EYE") {
     	 	x1 = document.getElementById("x" + curIndex);
 	     	y1 = document.getElementById("y" + curIndex);
+			eyes[curIndex-1].x = x;
+			eyes[curIndex-1].y = y;
 		} else {
 			x1 = document.getElementById("ball_x");
 	        y1 = document.getElementById("ball_y");
 			ballX = x - ballWidth/2;
-			ballY = y - ballHeight/2;
-			drawScene();
+			ballY = y - ballHeight/2;		
 		}
 
     x1.value = "" + x;
     y1.value = "" + y;
+
+	drawScene();
 }
 
 function setSelectedItem(type, val) {
